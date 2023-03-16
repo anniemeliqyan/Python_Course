096687031319784ee4ce2cd0f100a9b64f960543
anniemeliqyan <anniemeliqyan@gmail.com>
2023-03-16 17:58:04 +0400

    exercise3

---
+++ week3_folder/exercise3.py
@@ -0,0 +1,17 @@
+age = 65
+yearInDay = 365
+ageInDay = age * yearInDay
+print(ageInDay)
+
+
+age = 0
+yearInDay = 365
+ageInDay = age * yearInDay
+print(ageInDay)
+
+
+
+age = 20
+yearInDay = 365
+ageInDay = age * yearInDay
+print(ageInDay)
